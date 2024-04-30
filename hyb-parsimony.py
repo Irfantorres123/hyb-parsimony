e94318d6f5f7b52072d1bec8f22d96c3b6da489e
Tycho Bear <67806683+tycho-bear@users.noreply.github.com>
2024-04-30 15:25:40 -0400

    Currently modularizing the code for PSO. I'll update it to the hybrid algorithm by including the genetic algorithm operations next.

--- hyb-parsimony.py
+++ hyb-parsimony.py
@@ -2,7 +2,129 @@ import numpy as np
 from pyDOE2 import lhs
 
 
-# Supporting functions go here as needed
+class HybridParsimony:
+    """
+    Implements the HYB-PARSIMONY algorithm. This algorithm is based on particle
+    swarm optimization, but makes use of mutation and crossover from genetic
+    algorithms.
+    """
+
+    def __init__(self, f, D, num_particles, alpha, beta, max_iterations,
+                 lower_bounds, upper_bounds):
+        """
+        Initializes an instance of the HYB-PARSIMONY algorithm using the given
+        parameters.
+
+        :param f: the objective function to optimize
+        :param D: the number of dimensions to optimize this function in
+        :param num_particles: the population size
+        :param alpha: controls the influence of the global best value
+        :param beta: controls the influence of each particle's personal best value
+        :param max_iterations: the number of iterations to run the algorithm for
+        :param lower_bounds: the minimum values for each dimension
+        :param upper_bounds: the maximum values for each dimension
+        """
+
+        # initialize the parameters
+        self.f = f
+        self.D = D
+        self.num_particles = num_particles
+        self.alpha = alpha
+        self.beta = beta
+        self.max_iterations = max_iterations
+        self.lower_bounds = lower_bounds
+        self.upper_bounds = upper_bounds
+
+        # create the particles
+        self.particles = [Particle(f, D, alpha, beta, lower_bounds, upper_bounds)
+                          for _ in range(num_particles)]
+
+        # initialize the current global best value among the particles
+        self.global_best_pos = min(self.particles, key=lambda p: p.pos).pos
+        self.global_best_f = f(self.global_best_pos)
+
+        # todo one more line
+
+
+    def update_global_best(self, particle):
+        """
+        Updates the global best position if the given particle's best position
+        is better.
+
+        :param particle: the particle to check
+        :return: None
+        """
+
+        if particle.best_f < self.global_best_f:
+            self.global_best_pos = particle.best_pos
+            self.global_best_f = particle.best_f
+
+
+class Particle:
+    """
+    Represents a particle in the HYB-PARSIMONY algorithm. Each particle has a
+    position, velocity, and personal best value. Additionally, each particle has
+    an objective function to optimize, plus bounds for its position.
+    """
+
+    def __init__(self, f, D, alpha, beta, lower_bounds, upper_bounds):
+        """
+        Initializes a single particle.
+
+        :param f: the objective function to optimize
+        :param D: the number of dimensions to optimize this function in
+        :param alpha: controls the influence of the global best value
+        :param beta: controls the influence of each particle's personal best value
+        :param lower_bounds: the minimum values for each dimension
+        :param upper_bounds: the maximum values for each dimension
+        """
+
+        # initialize the parameters
+        self.f = f
+        self.D = D
+        self.alpha = alpha
+        self.beta = beta
+        self.lower_bounds = lower_bounds
+        self.upper_bounds = upper_bounds
+
+        # initialize the position and velocity from a latin hypercube sample
+        self.pos = (lhs(D, samples=1) * (upper_bounds - lower_bounds) + lower_bounds)
+        self.velocity = (lhs(D, samples=1) * (upper_bounds - lower_bounds) + lower_bounds)
+
+        # initialize the personal best values
+        self.best_pos = self.pos
+        self.best_f = f(self.best_pos)
+
+        # TODO: keep track of the most parsimonious model within tolerance
+        self.best_complexity_pos = self.pos
+        self.best_complexity_f = f(self.best_complexity_pos)
+
+
+    def update(self, global_best_pos):
+        """
+        Updates this particle's velocity and position.
+
+        :param global_best_pos: the global best position found so far
+        :return: None
+        """
+
+        epsilon1 = np.random.uniform()
+        epsilon2 = np.random.uniform()
+
+        # first update velocity, then the position, using this new velocity
+        self.velocity += self.alpha * epsilon1 * (global_best_pos - self.pos) + \
+                         self.beta * epsilon2 * (self.best_pos - self.pos)
+        self.pos += self.velocity
+        # stay within bounds
+        self.pos = np.clip(self.pos, self.lower_bounds, self.upper_bounds)
+
+        # then update this particle's best, as needed
+        f_pos = self.f(self.pos)
+        if f_pos < self.best_f:
+            self.best_f = f_pos
+            self.best_pos = self.pos
+
+
 
 
 
@@ -18,12 +140,12 @@ num_features = 5
 """The number of input features to the model."""
 
 # min and max values for the hyperparameters, can be individual for each
-min_param = np.array([0]*10)  # Adjust this based on the actual range
+lower_bounds = np.array([0]*10)  # Adjust this based on the actual range
 """
 The minimum values for the hyperparameters. Each hyperparameter can have a
 different minimum value.
 """
-max_param = np.array([1]*10)  # Adjust this as well
+upper_bounds = np.array([1]*10)  # Adjust this as well
 """
 The maximum values for the hyperparameters. Each hyperparameter can have a
 different maximum value.
@@ -41,77 +163,77 @@ tournament_size = 5
 mutation_rate = 0.01
 
 
-############################
-#  HYB-PARSIMONY algorithm
-############################
-
-# hyb-parsimony main algorithm
-def hyb_parsimony():
-    """
-    This is the implementation of the HYB-PARSIMONY algorithm (Algorithm 3) from
-    the paper.
-    :return:
-    """
-
-    # 1. Initialization of positions X^0 using a random and uniformly
-    # distributed Latin hypercube within the ranges of feasible values for each
-    # input parameter.
-    X = (lhs(num_hyperparameters + num_features, samples=population_size) *
-         (max_param - min_param) + min_param)  # can split these as needed
-
-    # 2. Initialization of velocities according to V^0 = (random_LHS(s, D) - X^0) / 2
-    V = ((lhs(num_hyperparameters + num_features, samples=population_size) *
-          (max_param - min_param) + min_param) - X) / 2
-
-    # 3. Main loop
-    for _ in range(num_generations):
-        """"""
-        # 4. Train each particle X_i(t) and validate with CV
-        # TODO - need to figure out how to do this part
-        #   Something like X[i] = train_and_validate(X[i], X, y)?
-
-        # 5. Fitness evaluation J and complexity evaluation M_c of each particle
-        # TODO - also need to figure this out
-
-        # 6. Update the position of the best fitness value achieved by the ith
-        # particle (X_hat_i), this particle's most parsimonious model (X_hat^p_i),
-        # and the global best found so far (X_hat_hat).
-
-
-        # (7. early stopping criterion - maybe ignore)
-        # (8. early stopping return - maybe ignore)
-        # (9. end early stopping check - maybe ignore)
-
-        # 10. Generation of new neighborhoods if X_hat_hat did not improve
-
-
-        # 11. Update each best position within a neighborhood (L_hat_i)
-
-
-        # 12. Select elitist population P_e for reproduction
-
-
-        # 13. Obtain a percentage (pcrossover) of worst individuals (P_w) to be
-        # substituted with crossover
-
-
-        # 14. Crossover P_e to substitute P_w with new individuals
-
-
-        # 15. Update positions and velocities of P_e  (TODO only update P_e?)
-
-
-        # 16. Mutation of a percentage of H (hyperparameters)
-
-
-        # 17. Mutation of a percentage of F (features)
-
-
-        # 18. Limitation of velocities and out-of-range positions (np.clip)
-
-
-    # 19. end main loop
-    # 20. Return X_hat_hat
+# ############################
+# #  HYB-PARSIMONY algorithm
+# ############################
+#
+# # hyb-parsimony main algorithm
+# def hyb_parsimony():
+#     """
+#     This is the implementation of the HYB-PARSIMONY algorithm (Algorithm 3) from
+#     the paper.
+#     :return:
+#     """
+#
+#     # 1. Initialization of positions X^0 using a random and uniformly
+#     # distributed Latin hypercube within the ranges of feasible values for each
+#     # input parameter.
+#     X = (lhs(num_hyperparameters + num_features, samples=population_size) *
+#          (upper_bounds - lower_bounds) + lower_bounds)  # can split these as needed
+#
+#     # 2. Initialization of velocities according to V^0 = (random_LHS(s, D) - X^0) / 2
+#     V = ((lhs(num_hyperparameters + num_features, samples=population_size) *
+#           (upper_bounds - lower_bounds) + lower_bounds) - X) / 2
+#
+#     # 3. Main loop
+#     for _ in range(num_generations):
+#         """"""
+#         # 4. Train each particle X_i(t) and validate with CV
+#         # TODO - need to figure out how to do this part
+#         #   Something like X[i] = train_and_validate(X[i], X, y)?
+#
+#         # 5. Fitness evaluation J and complexity evaluation M_c of each particle
+#         # TODO - also need to figure this out
+#
+#         # 6. Update the position of the best fitness value achieved by the ith
+#         # particle (X_hat_i), this particle's most parsimonious model (X_hat^p_i),
+#         # and the global best found so far (X_hat_hat).
+#
+#
+#         # (7. early stopping criterion - maybe ignore)
+#         # (8. early stopping return - maybe ignore)
+#         # (9. end early stopping check - maybe ignore)
+#
+#         # 10. Generation of new neighborhoods if X_hat_hat did not improve
+#
+#
+#         # 11. Update each best position within a neighborhood (L_hat_i)
+#
+#
+#         # 12. Select elitist population P_e for reproduction
+#
+#
+#         # 13. Obtain a percentage (pcrossover) of worst individuals (P_w) to be
+#         # substituted with crossover
+#
+#
+#         # 14. Crossover P_e to substitute P_w with new individuals
+#
+#
+#         # 15. Update positions and velocities of P_e  (TODO only update P_e?)
+#
+#
+#         # 16. Mutation of a percentage of H (hyperparameters)
+#
+#
+#         # 17. Mutation of a percentage of F (features)
+#
+#
+#         # 18. Limitation of velocities and out-of-range positions (np.clip)
+#
+#
+#     # 19. end main loop
+#     # 20. Return X_hat_hat
 
 
 
