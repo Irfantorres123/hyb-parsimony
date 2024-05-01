88e4fa8f756d059d973c9a7bf714c121b5deb640
Tycho Bear <67806683+tycho-bear@users.noreply.github.com>
2024-05-01 17:45:10 -0400

    Moved the files for the hybrid method into their own directory. Working on updating my PSO algorithm from homework 3 to use the genetic algorithm operations.

--- HYB_PARSIMONY/hyb_parsimony.py
+++ HYB_PARSIMONY/hyb_parsimony.py
@@ -79,6 +79,9 @@ class HybridParsimony:
             # Idea: update each particle, and then update the global best
             for particle in self.particles:
                 particle.update(self.global_best_pos)
+                # self.update_global_best(particle)
+
+            for particle in self.particles:
                 self.update_global_best(particle)
 
         return self.global_best_pos, self.global_best_f
@@ -176,11 +179,14 @@ class Particle:
         epsilon2 = np.random.uniform()
 
         # first update velocity, then the position, using this new velocity
-        self.velocity += self.alpha * epsilon1 * (global_best_pos - self.pos) + \
+        # self.velocity += self.alpha * epsilon1 * (global_best_pos - self.pos) + \
+        #                  self.beta * epsilon2 * (self.best_pos - self.pos)
+        self.velocity = 0.7 * self.velocity + self.alpha * epsilon1 * (global_best_pos - self.pos) + \
                          self.beta * epsilon2 * (self.best_pos - self.pos)
+
         self.pos += self.velocity
         # stay within bounds
-        self.pos = np.clip(self.pos, self.lower_bounds, self.upper_bounds)
+        self.pos = np.clip(self.pos, a_min=self.lower_bounds, a_max=self.upper_bounds)
 
         # then update this particle's best, as needed
         f_pos = self.f(self.pos)
@@ -301,3 +307,56 @@ class Particle:
 #     # 19. end main loop
 #     # 20. Return X_hat_hat
 
+
+
+def hyb_parsimony(f, D, n, alpha, beta, L, gamma, max_iterations, lower_bounds, upper_bounds):
+    """
+    Implements the hybrid parsimony algorithm.
+
+    :param f: the objective function to optimize
+    :param D: the number of dimensions to optimize this function in
+    :param n: the population size, number of particles
+    :param alpha: learning parameter #1 (alpha is close to beta is close to 2)
+    :param beta: learning parameter #2 (alpha is close to beta is close to 2)
+    :param max_iterations: the maximum number of iterations that the algorithm
+        should run for (this is the stopping criterion)
+    :param lower_bound: minimum value for xi
+    :param upper_bound: maximum value for xi
+    :return: x_best and f_best, the best solution found and its evaluation
+    """
+
+    # initialize locations xi and vi of n particles
+    # x = np.random.uniform(low=lower_bound, high=upper_bound, size=(n, D))
+    # v = np.zeros(shape=(n, D))
+
+    x = (lhs(D, samples=n, random_state=1234) * (upper_bounds - lower_bounds) + lower_bounds)
+    v = ((lhs(D, samples=n, random_state=1234) * (upper_bounds - lower_bounds) + lower_bounds) - x) / 2
+
+    g_star = min(x, key=f)  # best particle at time t
+    x_bests = {i: x[i] for i in range(n)}  # current best for particle i (i:x_best)
+    t = 0  # time, # of iterations
+
+    while t < max_iterations:
+        # for loop over all n particles and all d dimensions
+        for i in range(n):
+            e1 = np.random.uniform(size=D)
+            e2 = np.random.uniform(size=D)
+
+            # generate new velocity vi using equation (7.1)
+            v[i] = L * v[i] + alpha * e1 * (g_star - x[i]) + beta * e2 * (x_bests[i] - x[i])
+
+            # calculate new locations xi = xi + vi
+            x[i] = x[i] + v[i]
+            x[i] = np.clip(x[i], a_min=lower_bounds, a_max=upper_bounds)
+
+            # evaluate objective function at new location for x[i] and update
+            # the current best for this particle
+            if f(x[i]) < f(x_bests[i]):
+                x_bests[i] = x[i]
+
+        # find the current global best g*
+        g_star = min(x_bests.values(), key=f)
+        t += 1
+
+    # output the final results xi* and g*  (x_best, f_best?)
+    return g_star, f(g_star)
