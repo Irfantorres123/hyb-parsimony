f010abcd40e0e2d4ccf25bb4273bb76615c5cba2
Mohammed Irfan <irfantorres123@gmail.com>
2024-05-04 14:17:03 -0400

    added model evaluator

---
+++ model_eval.py
@@ -0,0 +1,92 @@
+from sklearn.svm import SVC
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+
+def FunctionModelWrapper(f):
+    """
+    Wrapper to convert a function to a model object with fit and score methods
+    """
+    
+
+
+
+class Evaluator:
+    def __init__(self,template,num_features,model,dataset):
+        """
+        params:
+        template: List of dictionaries containing info about that param including lower_bound, upper_bound,
+                  name and discreteValues. discreteValues is a list of possible values for the param.
+        num_features: Number of features in the dataset
+        model: Model to be used for evaluation. can be any scikit learn model
+        dataset: DataFrame. Dataset to be used for evaluation
+        if discreteValues is provided, then name is needed however lower_bound and upper_bound are not needed.
+        Example:
+        template:[{'lower_bound': 0, 'upper_bound': 1.0},{name:'C','discreteValues': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+        
+        """
+        self.template = template
+        self.num_features = num_features
+        self.num_hyperparameters = len(template) - num_features
+        self.model=model
+        self.dataset = dataset
+        for tmp in template:
+            if 'discreteValues' in tmp:
+                assert 'name' in tmp
+                tmp['discreteValues'] = np.array(tmp['discreteValues'])
+            
+
+    def clip(self,parameters:np.array):
+        """
+        Clip the parameters to their respective bounds and return them
+        params:
+        parameters: Numpy array containing the parameters to be clipped
+        """
+        for i in range(len(parameters)):
+            template = self.template[i]
+            if 'discreteValues' in template: # clip parameter to closest discrete value
+                discreteValues = template['discreteValues']
+                parameters[i] = discreteValues[np.argmin(np.abs(discreteValues - parameters[i]))]
+            else:
+                if parameters[i] < template['lower_bound']:
+                    parameters[i] = template['lower_bound']
+                elif parameters[i] > template['upper_bound']:
+                    parameters[i] = template['upper_bound']
+
+        return parameters
+    
+    def execute_agent(self,parameters:np.array):
+        """
+        Execute the model with the given parameters and return the score
+        params:
+        parameters: Numpy array containing the parameters to be used
+        """
+        hyperparameters = dict()
+        features = []
+        for i in range(len(parameters)):
+            if i < self.num_features:
+                features.append(parameters[i])
+            else:
+                hyperparameters[self.template[i]['name']] = parameters[i]
+        
+        model = self.model(**hyperparameters)
+        features = np.array(features)
+        features = self.clip(features)
+        features = features > 0.5
+        features = np.where(features)[0]
+        features = self.dataset.columns[features]
+        X = self.dataset[features].values
+        y = self.dataset.iloc[:,-1].values
+        model.fit(X,y)
+        return model.score(X,y)
+
+    def execute(self,agents:np.array,max_workers:int=4):
+        """
+        Execute the model with the given parameters and return the score
+        params:
+        parameters: Numpy array containing the parameters to be used
+        """
+        thread_pool = ThreadPoolExecutor(max_workers=max_workers)
+        results = []
+        for agent in agents:
+            results.append(thread_pool.submit(self.execute_agent,agent))
+        return results
