2db4aa63946c873b638ec2f53b58e1b75b597b68
Mohammed Irfan <irfantorres123@gmail.com>
2024-05-04 20:27:50 -0400

    added extra return value in evaluateagent

--- model_eval.py
+++ model_eval.py
@@ -89,7 +89,7 @@ class Evaluator:
         y = self.dataset.iloc[:,-1].values
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         model.fit(X_train,y_train)
-        return model.score(X_test,y_test)
+        return model.score(X_test,y_test), len(features)
 
     def execute(self,agents:List[np.array],max_workers:int=4):
         """
